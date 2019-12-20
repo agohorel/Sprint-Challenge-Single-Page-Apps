b9b99fff501165d2aa0666489a0b6b65e96c1826
agohorel <gohorelalex@gmail.com>
2019-12-20 11:21:06 -0500

    add axios get request and render results

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  return (
+    <div>
+      <p>name: {character.name}</p>
+      <p>species: {character.species}</p>
+      <p>origin: {character.origin.name}</p>
+      <p>status: {character.status}</p>
+    </div>
+  );
 }
