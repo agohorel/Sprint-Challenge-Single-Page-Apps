90ce40208860cefd1ab39f8ab13ea649a6db0f8d
agohorel <gohorelalex@gmail.com>
2019-12-20 11:13:29 -0500

    set up router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <WelcomePage></WelcomePage>
+          </Route>
+          <Route path="/characters">
+            <CharacterList></CharacterList>
+          </Route>
+        </Switch>
+      </Router>
     </main>
   );
 }
